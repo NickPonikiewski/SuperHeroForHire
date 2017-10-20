7f33999c3a5fe58f07238ed3dff6e058b6be346b
NickPonikiewski <nponikie@UMICH.EDU>
2017-10-20 11:25:06 -0400

    Added Base of game
    nothing in game yet

---
+++ Superhero for Hire/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int playerSpeed = 10;
+	public bool facingRight = true;
+	public float moveX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerMove ();
+	}
+	void PlayerMove(){
+		//controls
+		moveX = Input.GetAxis("Horizontal");
+		//player Dir
+		if (moveX < 0.0f && facingRight == false) {
+			FlipPlayer ();
+		} else if (moveX > 0.0f)
+		//phy
+	}
+	void FlipPlayer(){
+		
+	}
+}
